680dd5de4606f60e978797eb6ff23a0d17d5b955
iliyashev <iliya.shevolaev@gmail.com>
2025-02-15 15:52:44 +0300

    3.7

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/{id?}', function($id = 0) {
+//3.6
+Route::get('/user/{id?}', function ($id = 0) {
     return 'user id:' . $id;
 })->where('id', '[0-9]+');
+
+//3.7
+Route::get('/{year}/{month}/{day}', function ($year, $month, $day) {
+    $date = $year . '-' . $month . '-' . $day;
+
+    return date('l', strtotime($date));
+})->where([
+    'year' => '[0-9]+',
+    'month' => '[0-9]+',
+    'day' => '[0-9]+',
+]);
