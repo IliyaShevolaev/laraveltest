854495110b4142bc15f50616f7b54dfe69796597
iliyashev <iliya.shevolaev@gmail.com>
2025-02-15 15:41:46 +0300

    3.6

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{id?}', function($id = 0) {
+    return 'user id:' . $id;
+})->where('id', '[0-9]+');
